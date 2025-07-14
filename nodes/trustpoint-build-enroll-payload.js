aceccc38c224e595f139cf14bfe8b062a478f84a
ibrahimsambare <ibrahim.realmountaka@campus-schwarzwald.de>
2025-07-14 12:10:35 +0200

    Update trustpoint-build-enroll-payload.js

--- nodes/trustpoint-build-enroll-payload.js
+++ nodes/trustpoint-build-enroll-payload.js
@@ -1,25 +1,28 @@
-module.exports = function(RED) {
+module.exports = function (RED) {
     function TrustpointBuildEnrollPayload(config) {
         RED.nodes.createNode(this, config);
-        var node = this;
+        const node = this;
 
-        node.on('input', function(msg) {
+        node.on('input', function (msg) {
             if (!msg.payload || !msg.payload.csrDer || !msg.estUsername || !msg.estPassword || !msg.deviceId) {
                 node.error("Missing fields: estUsername, estPassword, deviceId or csrDer in msg", msg);
                 return;
             }
 
-            const payload = {
-                csr: msg.payload.csrDer.toString("base64"),
+            const csrDer = Buffer.isBuffer(msg.payload.csrDer)
+                ? msg.payload.csrDer
+                : Buffer.from(msg.payload.csrDer, 'binary');
+
+            msg.payload = {
+                csr: csrDer.toString("base64"),
                 username: msg.estUsername,
                 password: msg.estPassword,
                 deviceId: msg.deviceId
             };
 
-            msg.payload = payload;
             node.send(msg);
         });
     }
 
     RED.nodes.registerType("trustpoint-build-enroll-payload", TrustpointBuildEnrollPayload);
-}
+};
