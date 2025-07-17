139c5c2f75ce5f129576031363679cd759280f4d
ibrahimsambare <ibrahim.realmountaka@campus-schwarzwald.de>
2025-07-17 10:34:43 +0200

    Update trustpoint-cacerts.js

--- nodes/trustpoint-cacerts.js
+++ nodes/trustpoint-cacerts.js
@@ -6,7 +6,7 @@ module.exports = function (RED) {
     const node = this;
 
     node.on("input", function (msg, send, done) {
-      const estUrl = msg.estHost || "https://127.0.0.1/.well-known/est/cacerts";
+      const estUrl = msg.estHost || config.estHost || "https://127.0.0.1/.well-known/est/cacerts";
 
       const options = {
         method: "GET",
@@ -14,30 +14,33 @@ module.exports = function (RED) {
       };
 
       https.get(estUrl, options, (res) => {
-        let chunks = [];
+        const chunks = [];
 
         res.on("data", (chunk) => {
           chunks.push(chunk);
         });
 
         res.on("end", () => {
-          const raw = Buffer.concat(chunks);
-          const base64 = raw.toString("base64");
-          const pem = [
-            "-----BEGIN CERTIFICATE-----",
-            base64.match(/.{1,64}/g).join("\n"),
-            "-----END CERTIFICATE-----"
-          ].join("\n");} 
-
           try {
-        const pem = fs.readFileSync(certPath, "utf8");
-        msg.payload = {
-          certificate: pem,
-          deviceId: "ca-cert"
-        };
+            const raw = Buffer.concat(chunks);
+            const base64 = raw.toString("base64");
+            const pem = [
+              "-----BEGIN CERTIFICATE-----",
+              base64.match(/.{1,64}/g).join("\n"),
+              "-----END CERTIFICATE-----"
+            ].join("\n");
 
-        send(msg);
-        if (done) done();
+            msg.payload = {
+              certificate: pem,
+              deviceId: "ca-cert"
+            };
+
+            send(msg);
+            if (done) done();
+          } catch (err) {
+            node.error("Failed to process CA cert: " + err.message, msg);
+            if (done) done(err);
+          }
         });
       }).on("error", (err) => {
         node.error("Failed to fetch CA certs: " + err.message, msg);
